2a967aa0419e35181334756f5e5893d23e4b8841
Gerson Velasco <gvelassco@gmail.com>
2022-04-19 12:28:00 -0500

    hola

--- backend/App/views/asistencias_all.php
+++ backend/App/views/asistencias_all.php
@@ -3,7 +3,7 @@
     Asistencias - MUSA - GRUPO LAHE
 </title>
 <body class="g-sidenav-show  bg-gray-100">
-    <<?php echo $asideMenu;?>
+    <?php echo $asideMenu;?>
     <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
         <!-- Navbar -->
         <nav class="navbar navbar-main navbar-expand-lg position-sticky mt-4 top-1 px-0 mx-4 shadow-none border-radius-xl z-index-sticky" id="navbarBlur" data-scroll="true">
