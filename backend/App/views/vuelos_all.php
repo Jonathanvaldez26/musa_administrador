30272614d5ba85d7c5a924824c3401fd52a1a405
Gerson26 <gvelassco@gmail.com>
2022-04-27 22:10:52 -0500

    vuelos1

--- backend/App/views/vuelos_all.php
+++ backend/App/views/vuelos_all.php
@@ -218,7 +218,7 @@
                                     </div>
                                     <div class="card-body px-0 pt-0 pb-2">
                                         <div class="table-responsive p-0">
-                                            <table class="table align-items-center mb-0" id="vuelos_llegada_tabla">
+                                            <table class="table align-items-center mb-0" id="">
                                                 <thead>
                                                     <tr>
                                                         <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Nombre del Asistente</th>
